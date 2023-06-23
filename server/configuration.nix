ff9b995b29711b23f50c6bbdb5301f0d9b10ee40
drath <drathvader@wp.pl>
2023-06-24 01:16:24 +0200

    telegraf, move systemd back, lots of little chages

--- server/configuration.nix
+++ server/configuration.nix
@@ -4,19 +4,17 @@
 #		- telegraf + prometheus, or at least telegraf. needs access to everything anyway
 #		- maybe samba? nfs perchance? i like em in docker tho
 #		- portainer??? perchance
-# TODO section the config into files? sounds like pain. we also could:
-# TODO download (import, fetchurl) parts from github maybe? i could easily put backuppers on git, would be the same for servre and rpi too
-# i wonder if i can use fetchurl elsewhere
-# tbh i could just have a common repo for all nixos, have a common.nix file, then include it. but we'd need to copy it over to the target os too huh
-# TODO dkms for ryzen things, thats the only thing stopping me from moving. after that its just config cosmetics
-# we could detect if its ryzen and only build if it is
-# TODO completely disable getty login prompt at physical console, at least for rpi. its extra hardening and neat.
 
-# FOR RPI
-# TODO integrate config downloader/uploader/timer into rpi after its done (reads OS hostname and uploads to a correct folder based on that)
-# TODO autoupdates. are they stable enough?
+# HOW TF WAS KERNEL MODULES THIS EASY. this alone is enough of a reason to switch away from ubuntu 
+# TODO autodetect kernel version for modules?, though nixos uses LTS kernel so its not a huge deal
 
 { config, pkgs, ... }:
+
+let 
+	cfg = {
+		linuxVer = "linux_6_1";
+	};
+in
 
 {
 	imports = [
@@ -27,23 +25,27 @@
 
 	########### BASICS and NETWORKING ###########
 
+
 	boot = {
+		extraModulePackages = with pkgs.linuxKernel.packages.${cfg.linuxVer}; [ zenpower it87 ];
 		loader = {
 			systemd-boot.enable = true;
 			efi.canTouchEfiVariables = true;
 		};
-		kernelModules = [ "nfs" "nfsd" "tun" ]; # not sure if i really need tun
 		blacklistedKernelModules = [ "k10temp" ];
-		# supportedFilesystems = [ "zfs" "btrfs" ]; # zfs not strictly needed? need 2 test
+		kernelModules = [ "nfs" "nfsd" "tun" "zenpower" "it87" ]; # not sure if i really need tun
+		supportedFilesystems = [ "zfs" "btrfs" ]; # zfs not strictly needed? need 2 test
 		# zfs.extraPools = [ "zpool" ];
-		# kernelParams = [ "zfs.zfs_arc_min=0" "zfs.zfs_arc_max=1073741824" ]; # 1GB
+		kernelParams = [ "zfs.zfs_arc_min=0" "zfs.zfs_arc_max=1073741824" ]; # 1GB
+		tmp.cleanOnBoot = true;
 	};
 
 	networking = {
 		hostName = "nox-test"; # Define your hostname.
-		hostId = "5c932f77"; # for zfs, needs to be random
+		hostId = "5c932f77"; # for zfs, needs to be random TODO test importing if pool created with a diff id
 		networkmanager.enable = true;
 		firewall.checkReversePath = "loose"; # suggested for tailscale
+		firewall.enable = false; # yea no
 	};
 
 
@@ -103,21 +105,57 @@
 		vim
 		wget
 		htop
-		rclone # dont REALLY need it? since its defined in systemd pkgs anyway
+		# rclone # dont REALLY need it? since its defined in systemd pkgs anyway
 		neofetch
 		mergerfs
+		lm_sensors
 	];
 
 	services = {
+		# getty.autologinUser = "drath";
+		timesyncd.enable = true;
 		tailscale.enable = true; # still need to join by hand but thats probably fine
 		vscode-server.enable = true;
 		zfs.autoScrub.enable = true;
+
+		telegraf = {
+			enable = true;
+			extraConfig = {
+				inputs = {
+					mem = {};
+					cpu = {};
+					smart = {
+						path_smartctl = "${pkgs.smartmontools}/bin/smartctl";
+						path_nvme = "${pkgs.nvme-cli}/bin/nvme";
+					};
+					docker = {
+						endpoint =  "unix://run/docker.sock";
+  						perdevice = false;
+  						total = true;
+  						total_include = [ "cpu" "blkio" "network" ];
+					};
+					net = {
+						interval = "5s";
+  						interfaces = [ "enp*s[0-9]" ];
+					};
+					disk = {
+						ignore_fs = [ "tmpfs" "devtmpfs" "devfs" "iso9660" "overlay" "aufs" "squashfs" ];
+					};
+					diskio = {
+						device_tags = [ "ID_MODEL" "ID_SCSI_SERIAL" "ID_PATH" ];
+					};
+					zfs = {
+						poolMetrics = true;
+					};
+				};
+
+				outputs.prometheus_client = {
+					listen = ":9273";
+					metric_version = 2;
+				};
+			};
+		};
 	};
-
-	##### CHANGE/COMMENT OUT THESE BEFORE DEPLOYING FOR REAL #####
-	# services.getty.autologinUser = "drath";
-	##############################################################
-
 
 
 	systemd.user = {
@@ -140,8 +178,51 @@
 				description = "Download homedir files from google drive";
 				wantedBy = [ "default.target" ];
 			};
+
+			rclone-config-downloader = {
+				enable = true;
+				path = [ pkgs.curl pkgs.gzip ];
+				serviceConfig.Type = "oneshot";
+				unitConfig.ConditionPathExists = "!%S/rclone/rclone.conf";
+				serviceConfig.RemainAfterExit = "yes";
+				scriptArgs = "%S";
+				script = ''
+					mkdir -p $1/rclone
+					curl -sSL 192.168.1.1:82/?q=rclone.conf | base64 -d | gzip -d > $1/rclone/rclone.conf
+				'';
+				description = "Download homedir files from google drive";
+				wantedBy = [ "default.target" ]; # this is what actually makes it run on boot
+			};
+
+			backup-configs = {
+				enable = true;
+				path = [ pkgs.pigz pkgs.gnutar pkgs.rclone ];
+				serviceConfig.Type = "oneshot";
+				description = "Backup scripts and configs to google drive";
+				script = ''
+					cd ~; tar -cvf - configs | pigz | rclone rcat google:backup/$HOSTNAME/$HOSTNAME-configs.tar.gz
+					cd ~; tar -cvf - docker-compose | pigz | rclone rcat google:backup/$HOSTNAME/$HOSTNAME-docker-compose.tar.gz
+					cd ~; tar -cvf - --exclude=runme.sh *.sh *.yml .bashrc.d mc | pigz | rclone rcat google:backup/$HOSTNAME/$HOSTNAME-home-dir.tar.gz
+				'';
+			};
+		};
+
+		timers = {
+			backup-configs = {
+				enable = true; # maybe it would be better to explicitly include systemd.user.timers.backup-configs.enable = true; in each host
+				wantedBy = [ "timers.target" ];
+				description = "Timer to backup scripts and configs to google drive";
+				timerConfig = {
+					OnCalendar = "*-*-* 0,6,12,18:00:00";
+					Unit = "backup-configs.service";
+				};
+			};
 		};
 	};
 
 	system.stateVersion = "23.05";
+
+
+	##### TESTING FLAKES BELOW
+	nix.settings.experimental-features = [ "nix-command" "flakes" ];
 }
