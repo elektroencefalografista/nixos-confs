e92312b5d011fb4dd0fef4ecb36357d19ea42ddd
drath <drathvader@wp.pl>
2023-06-23 11:41:42 +0200

    test?

---
+++ common.nix
@@ -0,0 +1,20 @@
+{
+	systemd.user = {
+		services = {
+			rclone-config-downloader = {
+				enable = true;
+				path = [ pkgs.curl pkgs.gzip ];
+				serviceConfig.Type = "oneshot";
+				unitConfig.ConditionPathExists = "!%S/rclone/rclone.conf";
+				serviceConfig.RemainAfterExit = "yes";
+				scriptArgs = "%S";
+				script = ''
+					mkdir -p $1/rclone
+					curl -sSL 192.168.1.1:82/?q=rclone.conf | base64 -d | gzip -d > $1/rclone/rclone.conf
+				'';
+				description = "Download homedir files from google drive";
+				wantedBy = [ "default.target" ]; # this is what actually makes it run on boot
+			};
+		};
+	};
+}
