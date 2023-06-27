1539349dc5ae1bf428ae873936e4bfbb2b5dd9d1
drath <drathvader@wp.pl>
2023-06-27 20:08:41 +0200

    make it use variables, set up backups to gdrive

--- rpi/rpi.nix
+++ rpi/rpi.nix
@@ -1,8 +1,17 @@
 # TODO figure out how to backup/provision basic settings for pihole
 # TODO flask gcs downloader with nix insted of docker. time to learn flakes ig
+# TODO gonna need rclone downloader. wont work on first boot when docker isnt setup, but it will work eventually
 
 
 { config, pkgs, ... }:
+
+let 
+	cfg = {
+		hostname = "rpi";
+		username = "drath";
+		mem.swapSize = 512;
+	};
+in
 
 {	
 	imports = [
@@ -25,15 +34,15 @@
 		}];
 		defaultGateway = "192.168.1.254";
 		nameservers = [ "127.0.0.1" ];
-		hostName = "rpi";
+		hostName = cfg.hostname;
 		# firewall.enable = false; # yea no
 		firewall.allowedTCPPorts = [ 53 67 80 82 3000 ]; # port 81 still works?
 		firewall.allowedUDPPorts = [ 53 67 547 ];
 	};
 
 	swapDevices = [{
-		device = "/var/lib/swap.img";
-		size = 1*1024; # 1GB
+		device = "/var/lib/swapfile";
+		size = cfg.mem.swapSize;
 	}];
 
 	environment.systemPackages = with pkgs; [
@@ -71,6 +80,39 @@
 
 		};
 	};
+
+	systemd = {
+		services = {
+			backup-configs = {
+				enable = true;
+				path = [ pkgs.pigz pkgs.gnutar pkgs.rclone ];
+				serviceConfig = {
+					Type = "oneshot";
+					User = cfg.username;
+				};
+				description = "Backup scripts and configs to google drive";
+				script = ''
+					cd ~; tar -cvf - configs | pigz | rclone rcat google:backup/$HOSTNAME/$HOSTNAME-configs.tar.gz
+					cd ~; tar -cvf - docker-compose | pigz | rclone rcat google:backup/$HOSTNAME/$HOSTNAME-docker-compose.tar.gz
+					cd ~; tar -cvf - --exclude=runme.sh *.sh *.yml .bashrc.d mc | pigz | rclone rcat google:backup/$HOSTNAME/$HOSTNAME-home-dir.tar.gz
+				'';
+			};
+		};
+
+		timers = {
+			backup-configs = {
+				enable = true;
+				wantedBy = [ "timers.target" ];
+				description = "Timer to backup scripts and configs to google drive";
+				requires = [ "backup-configs.service" ]; # fuck you
+				timerConfig = {
+					OnCalendar = "*-*-* 0,6,12,18:00:00";
+					Unit = "backup-configs.service";
+				};
+			};
+		};
+	};
+
 
 	virtualisation.docker.enable = true;
 	system.stateVersion = "23.05";
