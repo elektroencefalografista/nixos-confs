3a89914d1c1ac6184e321846f6b093c1e8c0d944
drath <drathvader@wp.pl>
2023-09-29 19:58:38 +0200

    lets have samba in docker after all

--- server/unused.nix
+++ server/unused.nix
@@ -4,11 +4,39 @@ i18n.extraLocaleSettings.LC_TIME = "pl_PL.UTF-8";	# just sets the time format
 console.keyMap = "pl2";								# not using console all that much
 
 		# we have samba at home
-		services.samba = {
+		samba-wsdd.enable = true;
+		samba = {
 			enable = true;
+			openFirewall = true;
+			securityType = "user";
+			extraConfig = ''
+				server min protocol = SMB3
+				workgroup = WORKGROUP
+				server string = server
+    			netbios name = server
+    			security = user 
+    			use sendfile = yes
+    			hosts allow = 192.168.1. 100. 127.0.0.1 localhost
+    			hosts deny = 0.0.0.0/0
+    			guest account = nobody
+    			map to guest = bad user
+			'';
 			shares = {
-				public = {
+				anime = {
 					path = "/mnt/anime";
+					"valid users" = "drath";
+					"guest ok" = "no";
+					"read only" = "no";
+					browseable = "yes";
+					"create mask" = "0644";
+   				    "directory mask" = "0755";
+      				"force user" = "drath";
+      				# "force group" = "drath";
+				};
+				movies = {
+					path = "/mnt/anime/Jellyfin";
+					"guest ok" = "yes";
+					"read only" = "yes";
 					browseable = "yes";
 				};
 			};
@@ -28,58 +56,4 @@ console.keyMap = "pl2";								# not using console all that much
 				"--storage.tsdb.path=/var/promdb/"
 			];
 		};
-
-	########### FILESYSTEMS ########### should i move zfs here?
-
-	boot.zfs.extraPools = [ "zpool" ];
-
-	fileSystems = {
-		"/mnt/mfs_share" = {
-			device = "/dev/disk/by-uuid/cdb15f8d-7a83-4b33-aaf7-e4147261900a";
-			fsType = "btrfs";
-			options = [ 
-				"relatime" 
-				"nofail"
-				"defaults"
-				"x-systemd.mount-timeout=15" ];
-		};
-
-		"/mnt/mfs_anime" = {
-			device = "/dev/disk/by-uuid/f4ecfce7-0ff2-4f1f-9709-de874618fe58";
-			fsType = "btrfs";
-			options = [ 
-				"relatime" 
-				"nofail"
-				"defaults"
-				"x-systemd.mount-timeout=15" ];
-		};
-
-		"/mnt/mfs_purple" = {
-			device = "/dev/disk/by-uuid/557885a9-7107-43d2-bab8-109a36b351af";
-			fsType = "ext4";
-			options = [ 
-				"relatime" 
-				"nofail"
-				"defaults"
-				"x-systemd.mount-timeout=15" ];
-		};
-
-		"/mnt/anime" = {
-			device = "/mnt/mfs_*:/mnt/zpool";
-			fsType = "fuse.mergerfs";
-			options = [ 
-				"defaults" 
-				"nonempty"
-				"allow_other"
-				"use_ino"
-				"category.create=msplfs"
-				"dropcacheonclose=true"
-				"minfreespace=10G"
-				"fsname=mfs_pool"
-				"nofail" ];
-			depends = [ "/mnt/zpool" "/mnt/mfs_anime" ];
-			noCheck = true;
-		};
-
-	};
 }
