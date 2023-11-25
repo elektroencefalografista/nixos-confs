2f85394fee272e04a8b78e2f38ec582cbd332805
drath <drathvader@wp.pl>
2023-11-25 16:27:41 +0100

    cleaner, and if i do use linux i can rewrite it

--- rebuild.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-HOST=drath@192.168.1.178
-
-scp -o StrictHostKeychecking=no server/configuration.nix ${HOST}:~/.tmp
-ssh -o StrictHostKeychecking=no ${HOST} 'bash -s' << EOF
-	sudo cp ~/.tmp /etc/nixos/configuration.nix
-	sudo nixos-rebuild switch
-EOF
