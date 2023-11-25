de95a448400e68cd3e786f6603ae8cef21a3a5fe
drath <drathvader@wp.pl>
2023-11-25 18:19:38 +0100

    old config downloaders

--- server/unused.nix
+++ server/unused.nix
@@ -56,4 +56,42 @@ console.keyMap = "pl2";								# not using console all that much
 				"--storage.tsdb.path=/var/promdb/"
 			];
 		};
+
+		user = {
+			services = {
+				oneshot-config-downloader = {
+					enable = true;
+					path = [ pkgs.pigz pkgs.gnutar pkgs.rclone ];
+					after = [ "rclone-config-downloader.service" ];
+					serviceConfig.Type = "oneshot";
+					unitConfig.ConditionPathExists = "!%S/%N.stamp";
+					serviceConfig.RemainAfterExit = "yes";
+					scriptArgs = "%S %N ${cfg.oneshotConfigDownloaderSource}";
+					script = ''
+						mkdir -p $1
+						rclone cat google:backup/$3/$3-docker-compose.tar.gz | pigz -d | tar -x -C ~ && \
+						rclone cat google:backup/$3/$3-home-dir.tar.gz | pigz -d | tar -x -C ~ && \
+						rclone cat google:backup/$3/$3-configs.tar.gz | pigz -d | tar -x -C ~ && \
+						touch $1/$2.stamp
+					'';
+					description = "Download homedir files from google drive";
+					wantedBy = [ "default.target" ];
+				};
+
+				rclone-config-downloader = {
+					enable = true;
+					path = [ pkgs.curl pkgs.gzip ];
+					serviceConfig.Type = "oneshot";
+					unitConfig.ConditionPathExists = "!%S/rclone/rclone.conf";
+					serviceConfig.RemainAfterExit = "yes";
+					scriptArgs = "%S";
+					script = ''
+						mkdir -p $1/rclone
+						curl -sSL 192.168.1.1:82/?q=rclone.conf | base64 -d | gzip -d > $1/rclone/rclone.conf
+					'';
+					description = "Download rclone config";
+					wantedBy = [ "default.target" ]; # this is what actually makes it run on boot
+				};
+			};
+		};
 }
