361ec60f669a3a9cddaa786cf438d2884cdcf22c
DrathVader <drathvader@wp.pl>
2024-03-04 14:41:42 +0100

    works

--- server/configuration.nix
+++ server/configuration.nix
@@ -320,7 +320,7 @@ in
 					mqtt = {
 						servers = [ "192.168.1.1:1883" ];
 						qos = 0;
-						topic = "telegraf/{{ .Hostname }}/{{ .PluginName }}{{.Tag \"path\" }}{{.Tag \"interface\" }}";
+						topic = "telegraf/{{ .Hostname }}/{{ .PluginName }}/{{.Tag \"path\" }}{{.Tag \"interface\" }}";
 						client_id = "telefraf";
 						username = "telegraf";
 						password = "ukVDMfkJX7tjh/sR7Vl6";
